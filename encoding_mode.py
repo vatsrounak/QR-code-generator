1e653c39e00d831f610b05f27784f9af8f282d0b
vatsrounak <rounakvats01@gmail.com>
2024-04-17 14:20:03 +0530

    encoding_mode_modified

--- encoding_mode.py
+++ encoding_mode.py
@@ -8,14 +8,16 @@ def determine_encoding_mode(input_data):
         return None
     
     numeric_chars = set("0123456789")
-    alphanumeric_chars = set("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:")   
-    kanji_chars = set("漢字")
+    alphanumeric_chars = set("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz$%*+-./:")   
+    # kanji_chars = set("漢字") 
+    # rather lets use the range here, because there are lots of kanji characters
+    kanji_chars_range = set(chr(i) for i in range(0x4E00, 0xA000))
     
     if all(char in numeric_chars for char in input_data):
         return "numeric"
     elif all(char in alphanumeric_chars for char in input_data):
         return "alphanumeric"
-    elif all(char in kanji_chars for char in input_data):
+    elif any(char in kanji_chars_range for char in input_data):
         return "kanji"
     else:
         return "byte"
